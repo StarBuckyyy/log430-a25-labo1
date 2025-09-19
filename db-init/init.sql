d54775e6d1e3d578d9fb73abccf115e34e7d6582
StarBuckyyy <validzicgoran@gmail.com>
2025-09-19 09:46:34 -0400

    nouvel essai 4

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,16 +1,16 @@
--- Créer le tableau Users
+CREATE DATABASE IF NOT EXISTS mydb;
+USE mydb;
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(80) NOT NULL,
     email VARCHAR(80) NOT NULL
 );
 
--- Créer des enregistrements dans Users
 INSERT INTO users (name, email) VALUES
 ('Ada Lovelace', 'alovelace@example.com'),
 ('Adele Goldberg', 'agoldberg@example.com'),
 ('Alan Turing', 'aturing@example.com');
-
 
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -18,4 +18,3 @@ CREATE TABLE IF NOT EXISTS products (
     brand VARCHAR(20) NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
-
